2e07bc617cf138273c27f9ce692c354a61c0d05e
imanisul <ianisul768@gmail.com>
2026-06-23 18:45:11 +0530

    Fix: convert frontend and backend submodules to regular tracked directories

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+
+import {createServer} from "http";
+import {Server} from "socket.io";
+import { YSocketIO } from "y-socket.io/dist/server";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const Port = process.env.PORT ;
+
+
+const app = express();
+const httpServer = createServer(app);
+
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const ySocketIO = new YSocketIO(io)
+ySocketIO.initialize();
+
+httpServer.listen(Port, () => {
+  console.log(`Server is running on port ${Port}`);
+});
+
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Server is running",
+    status: "success",
+  })
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    message: "Server is healthy",
+    status: "success",
+  })
+});
